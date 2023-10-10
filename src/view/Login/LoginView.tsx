a6135a3cdb3deed1ae08ebe5c3e8432c09ed4928
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-10-10 18:39:29 -0400

    Installed Formik and Yup
    Implemented Formik in Login Form

--- src/view/Login/LoginView.tsx
+++ src/view/Login/LoginView.tsx
@@ -1,10 +1,8 @@
+import { Formik } from 'formik';
 import React from 'react'
 
 export default function LoginView() {
 
-  const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  }
   return (
     <div
       className='max-w-2xl mx-auto shadow-md px-8 py-8 mt-8'
@@ -12,27 +10,49 @@ export default function LoginView() {
       <h2 className='text-2xl'>Inicia Sesión</h2>
       <p>Conéctate con amigos alrededor del mundo con Alfabook.</p>
 
-      <form
-        onSubmit={handlerSubmit}
-        className='flex flex-col items-center gap-4 mt-4'
+      <Formik
+        initialValues={{
+          email: '',
+          password: ''
+        }}
+        onSubmit={(values, { resetForm }) => {
+          console.log('Enviado', values)
+          resetForm()
+        }}
       >
-        <input 
-          type="email" 
-          placeholder='Correo' 
-          className='w-full border-b-2 border-thirdBlue py-2' 
-        />
-        <input 
-          type="password" 
-          placeholder='Contraseña' 
-          className='w-full border-b-2 border-thirdBlue py-2' 
-        />
-        <button 
-          type="submit"
-          className='flex items-center gap-2 bg-firstBlue text-whiteColor py-1 px-4 rounded-md font-bold'  
-        >
-          Iniciar Sesión
-        </button>
-      </form>
+        {({ values, handleSubmit, handleChange, handleBlur}) => 
+
+          <form
+            onSubmit={handleSubmit}
+            className='flex flex-col items-center gap-4 mt-4'
+          >
+            <input 
+              type="email" 
+              placeholder='Correo' 
+              className='w-full border-b-2 border-thirdBlue py-2' 
+              value={values.email}
+              name='email'
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <input 
+              type="password" 
+              placeholder='Contraseña' 
+              className='w-full border-b-2 border-thirdBlue py-2' 
+              value={values.password}
+              name='password'
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <button 
+              type="submit"
+              className='flex items-center gap-2 bg-firstBlue text-whiteColor py-1 px-4 rounded-md font-bold'  
+            >
+              Iniciar Sesión
+            </button>
+          </form>
+        }
+      </Formik>
     </div>
   )
 
